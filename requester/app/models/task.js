d0ecc7d8a557367a5247f7c7d7b79c2edda1c71a
joostrothweiler <joostrothweiler@gmail.com>
2017-03-15 13:40:36 +0100

    something that doesnt work

--- requester/app/models/task.js
+++ requester/app/models/task.js
@@ -2,7 +2,19 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var TaskSchema   = new Schema({
-	name: String
+	_id: integer,
+	name: String,
+	requester_id: ObjectID,
+	sources: [
+		{
+source_id : Number, 
+parameters: { } // e.g. hashtags [],
+},
+	]
+	questions: [
+		question: String,
+		response_type: String,
+],
 });
 
 module.exports = mongoose.model('Task', TaskSchema);
