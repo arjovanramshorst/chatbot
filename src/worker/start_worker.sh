371d45f6c1aec577c356ccbfbd6310c5bdb1eb04
janceesvansenden <jancees_van_senden@hotmail.com>
2017-03-27 17:07:38 +0200

    moved worker to src folder

---
+++ src/worker/start_worker.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+gnome-terminal -e 'nodemon server.js'
+# gnome-terminal --window-with-profile=Slick --working-directory=/home/jancees/Documents/Courses/Information Retrieval/chatbot/worker/rasa_nlu -e 'python -m rasa_nlu.server -c config.json --server_model_dir=./Models/model_20170326-131948'
